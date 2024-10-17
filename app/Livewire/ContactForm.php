be295d7e629c57014df11abf3dea4a48658a5b29
RMike1 <71224316+RMike1@users.noreply.github.com>
2024-10-17 17:26:29 +0200

    contact form validation fixed...

--- app/Livewire/ContactForm.php
+++ app/Livewire/ContactForm.php
@@ -4,36 +4,38 @@ namespace App\Livewire;
 
 use App\Models\Contact;
 use Livewire\Component;
-use Livewire\Attributes\Validate;
+use Livewire\Attributes\Rule;
 
 class ContactForm extends Component
 {
 
 
-    #[Validate('required', message: 'Please provide your name')]
-    #[Validate('min:3', message: 'The name is too short. It must be at least 3 characters long')]
-    #[Validate('max:50', message: 'The name is too long. It can have a maximum of 50 characters')]
-    #[Validate('regex:/^[a-zA-Z\s]+$/', message: 'The name can only contain letters and spaces')]
-    public $name = '';
+        #[Rule('required', message: 'Please provide your name')]
+        #[Rule('min:3', message: 'The name is too short. It must be at least 3 characters long')]
+        #[Rule('max:50', message: 'The name is too long. It can have a maximum of 50 characters')]
+        #[Rule('regex:/^[a-zA-Z\s]+$/', message: 'The name can only contain letters and spaces')]
+        public $name = '';
 
-    #[Validate('required', message: 'Please provide your email address')]
-    #[Validate('email', message: 'Please enter a valid email address')]
-    #[Validate('min:3', message: 'The email is too short. It must be at least 3 characters long')]
-    #[Validate('max:50', message: 'The email is too long. It can have a maximum of 50 characters')]
-    public $email = '';
+        #[Rule('required', message: 'Please provide your email address')]
+        #[Rule('email', message: 'Please enter a valid email address')]
+        #[Rule('min:3', message: 'The email is too short. It must be at least 3 characters long')]
+        #[Rule('max:50', message: 'The email is too long. It can have a maximum of 50 characters')]
+        public $email = '';
 
-    #[Validate('required', message: 'Please provide a message')]
-    #[Validate('min:3', message: 'The message is too short. It must be at least 3 characters long')]
-    #[Validate('max:1000', message: 'The message is too long. It can have a maximum of 1000 characters')]
+        #[Rule('required', message: 'Please provide a message')]
+        #[Rule('min:3', message: 'The message is too short. It must be at least 3 characters long')]
+        #[Rule('max:1000', message: 'The message is too long. It can have a maximum of 1000 characters')]
     public $message = '';
-
 
     public function ContactUs(){
 
         $validated=$this->validate();
         Contact::create($validated);
+        session()->flash('success', 'Thanks for reaching out,'.$this->name.'!. We appreciate your message and will respond soon.!');
         $this->reset();
-        session()->flash('success', 'created successfully!!');
+        $this->resetValidation(['name','email','message']);
+        // $this->resetErrorBag();
+        // $this->resetValidation();
     }
 
     public function render()
