8cae1ee017721ceeabd6ec5a9dd4810825962683
RMike1 <71224316+RMike1@users.noreply.github.com>
2024-10-24 00:42:14 +0200

    ..

---
+++ app/Filament/Resources/SubscriberResource.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\SubscriberResource\Pages;
+use App\Filament\Resources\SubscriberResource\RelationManagers;
+use App\Models\Subscriber;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class SubscriberResource extends Resource
+{
+    protected static ?string $model = Subscriber::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    protected static ?string $slug = 'subscribers';
+    protected static ?string $navigationLabel = 'Subscribers';
+    protected static ?string $modelLabel = 'Subscribers';
+    protected static ?string $pluralLabel = 'Subscribers';
+    protected static ?int $navigationSort = 22;
+    protected static ?string $navigationGroup = 'Subscribers';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('counter')
+                ->label('No.')
+                ->getStateUsing(function ($rowLoop, $record) {
+                    return $rowLoop->iteration;
+                })
+                ->sortable(false),
+                Tables\Columns\TextColumn::make('email')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('created_at')->label('Subscribed At')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: false),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\DeleteAction::make(),
+                Tables\Actions\ForceDeleteAction::make(),
+                Tables\Actions\RestoreAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                    Tables\Actions\ForceDeleteBulkAction::make(),
+                    Tables\Actions\RestoreBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListSubscribers::route('/'),
+        ];
+    }
+    public static function getEloquentQuery(): Builder
+    {
+        return parent::getEloquentQuery()
+            ->withoutGlobalScopes([
+                SoftDeletingScope::class,
+            ]);
+    }
+}
