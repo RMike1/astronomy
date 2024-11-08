924bf4ffec1c73c6b0e54d3b45822bc4bdb4dfac
RMike1 <71224316+RMike1@users.noreply.github.com>
2024-11-08 11:37:27 +0200

    homepage section observer added..

--- app/Models/HomePageSection.php
+++ app/Models/HomePageSection.php
@@ -2,10 +2,14 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Observers\HomePageSectionObserver;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Attributes\ObservedBy;
 
+
+#[ObservedBy(HomePageSectionObserver::class)]
 class HomePageSection extends Model
 {
     use HasFactory;
---
+++ app/Observers/HomePageSectionObserver.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\User;
+use App\Models\HomePageSection;
+use Illuminate\Support\Facades\Log;
+use Filament\Notifications\Notification;
+
+class HomePageSectionObserver
+{
+    /**
+     * Handle the HomePageSection "created" event.
+     */
+    public function created(HomePageSection $homePageSection): void
+    {
+        $recipients=User::role('super_admin')->get();
+        foreach($recipients as $recipient){
+            Notification::make()
+            ->title('New Content created')
+            ->body('View created content. <u> <a href="' . route('filament.admin.resources.home-section.view',$homePageSection->id) . '">here</a></u>')
+            ->sendToDatabase($recipient);
+
+        }
+    }
+
+    /**
+     * Handle the HomePageSection "updated" event.
+     */
+    public function updated(HomePageSection $homePageSection): void
+    {
+        $recipients=User::role('super_admin')->get();
+        foreach($recipients as $recipient){
+            Notification::make()
+            ->title('Content updated')
+            ->body('View updated content. <u> <a href="' . route('filament.admin.resources.home-section.view',$homePageSection->id) . '">here</a></u>')
+            ->sendToDatabase($recipient);
+        }
+    }
+
+    /**
+     * Handle the HomePageSection "deleted" event.
+     */
+    public function deleted(HomePageSection $homePageSection): void
+    {
+        //
+    }
+
+    /**
+     * Handle the HomePageSection "restored" event.
+     */
+    public function restored(HomePageSection $homePageSection): void
+    {
+        //
+    }
+
+    /**
+     * Handle the HomePageSection "force deleted" event.
+     */
+    public function forceDeleted(HomePageSection $homePageSection): void
+    {
+        //
+    }
+}
