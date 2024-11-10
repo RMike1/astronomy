24fd1dbd960654793fbc47b681cf3e6ab47eb033
RMike1 <71224316+RMike1@users.noreply.github.com>
2024-11-10 13:17:41 +0200

    nav issue fixed..

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -11,31 +11,43 @@
                     <li class="nav__menu lg:py-7" :class="{ 'lg:!py-4': stickyMenu }">
                         <a href="#Home"
                             class="relative text-white/80 text-sm py-1.5 px-4 border border-transparent hover:text-white hover:nav-gradient"
-                            :class="{ '!text-white nav-gradient': page === 'home'}">Home</a>
+                            :class="{ '!text-white nav-gradient': page === 'home' && activeSection === 'Home' }">
+                            Home
+                        </a>
                     </li>
                     <li class="nav__menu lg:py-7" :class="{ 'lg:!py-4': stickyMenu }">
                         <a href="#Observatories"
                             class="relative text-white/80 text-sm py-1.5 px-4 border border-transparent hover:text-white hover:nav-gradient"
-                            :class="{ '!text-white nav-gradient': page === 'home' && activeSection === 'Observatories' }">Observatories</a>
+                            :class="{ '!text-white nav-gradient': page === 'home' && activeSection === 'Observatories' }">
+                            Observatories
+                        </a>
                     </li>
                     <li class="nav__menu lg:py-7" :class="{ 'lg:!py-4': stickyMenu }">
                         <a href="#Services"
                             class="relative text-white/80 text-sm py-1.5 px-4 border border-transparent hover:text-white hover:nav-gradient"
-                            :class="{ '!text-white nav-gradient': page === 'home' && activeSection === 'Services' }">Services</a>
+                            :class="{ '!text-white nav-gradient': page === 'home' && activeSection === 'Services' }">
+                            Services
+                        </a>
                     </li>
                     <li class="nav__menu lg:py-7" :class="{ 'lg:!py-4': stickyMenu }">
                         <a href="{{ route('about') }}"
-                            class="relative text-white/80 text-sm py-1.5 px-4 border border-transparent hover:text-white hover:nav-gradient">About
-                            Us</a>
+                            class="relative text-white/80 text-sm py-1.5 px-4 border border-transparent hover:text-white hover:nav-gradient"
+                            :class="{ '!text-white nav-gradient': page === 'about' && !activeSection }">
+                            About Us
+                        </a>
                     </li>
                     <li class="nav__menu lg:py-7" :class="{ 'lg:!py-4': stickyMenu }">
                         <a href="#Contact"
                             class="relative text-white/80 text-sm py-1.5 px-4 border border-transparent hover:text-white hover:nav-gradient"
-                            :class="{ '!text-white nav-gradient': page === 'home' && activeSection === 'Contact' }">Contact
-                            Us</a>
+                            :class="{ '!text-white nav-gradient': page === 'home' && activeSection === 'Contact' }">
+                            Contact Us
+                        </a>
                     </li>
                 </ul>
             </nav>
+            
+
+
         </div>
     </x-slot>
 
